cf9f73a689e1afec4c6be7259c2890685780f2db
Rafaelhss <rafaelhss@gmail.com>
2023-09-30 00:33:44 -0300

    Add files via upload
    ver 1

--- src/script.js
+++ src/script.js
@@ -1,3 +1,25 @@
+// function clearAll(){
+// 	[...document.querySelectorAll('li')].forEach(function(li){
+		
+// 		li.classList.add('big')	
+// 		li.classList.remove('done')					
+// 	}																   
+// 	)
+// }
+
+// function checkDay(){
+
+// 	var day = new Date().getDate();
+// 	const dayReg = localStorage.getItem("day");
+
+// 	if(day != dayReg){
+// 		clearAll();
+// 		localStorage.setItem("day", day);	
+// 	}
+// }
+// checkDay();
+
+
 function changeMeal(meal){
 	if(Array.from(meal.querySelectorAll('li')).filter(ch => {return ch.classList.contains('done')}).length==0){
 		meal.querySelectorAll('li').forEach(li => li.classList.add('big'))
@@ -5,10 +27,23 @@ function changeMeal(meal){
 	
 	
 }
-[...document.querySelectorAll('li')].forEach(li => 
+
+[...document.querySelectorAll('li')].filter(li => {return li.id != 'clear'}).forEach(li => 
   li.addEventListener('click', e => (e.stopPropagation(), li.classList.toggle('done'),
 changeMeal(li.closest('.meal')) 
 																		 
 )//lambta
 )//evt listener
 );//fore
+
+
+
+document.querySelector('#clear').addEventListener('click', function(){
+	[...document.querySelectorAll('li')].filter(li => {return li.id != 'clear'}).forEach(function(li){
+		
+		li.classList.add('big')	
+		li.classList.remove('done')					
+	}																   
+	  )//lambta
+});
+																		   
